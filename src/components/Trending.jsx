ee6cd5f360cbd22816ac4501627a2f5f965cb5c1
Daria Nebylitsa <daranebylitsa@gmail.com>
2023-08-15 18:36:11 +0300

    added trending info

--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -12,7 +12,38 @@ const Trending = () => {
       console.log(response.data.coins);
     });
   }, [url]);
-  return <div>Trending</div>;
+
+  return (
+    <div className='my-12 py-8 rounded-div'>
+      <h1 className='py-4 font-bold text-2xl'>Trending Coins</h1>
+      <div className='grid md:grid-cols-2 gap-4'>
+        {trending.map((coin) => (
+          <div
+            key={coin.item.coin_id}
+            className='p-4 rounded-div hover:scale-105 ease-in duration-300'
+          >
+            <div className='w-full flex justify-between items-center'>
+              <div className='flex items-center'>
+                <img className='mr-4 rounded-full' src={coin.item.small} alt='/' />
+                <div>
+                  <p className='font-bold hidden md:block'>{coin.item.name}</p>
+                  <p className='font-bold md:font-normal'>{coin.item.symbol}</p>
+                </div>
+              </div>
+              <div className='flex items-center'>
+                <img
+                  className='w-4 mr-2'
+                  src='https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579'
+                  alt='/'
+                />
+                <p>{coin.item.price_btc.toFixed(7)}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Trending;
